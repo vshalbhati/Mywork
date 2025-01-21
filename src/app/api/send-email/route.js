5fba5ab683565fbeaece058b33609b1850bebcbb
vshalbhati <vishalbhati294@gmail.com>
2025-01-21 13:35:25 +0530

    made changes for deployement to vercel

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -1,4 +1,3 @@
-// src/app/api/send-email/route.js
 import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
@@ -7,8 +6,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: process.env.NEXT_PUBLIC_EMAIL_USER,
+    pass: process.env.NEXT_PUBLIC_EMAIL_PASS
   }
 });
 
